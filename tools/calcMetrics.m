0c48e52b618d641ff8230d6e289786c631fe1c6c
John Fox <johnfox17@gmail.com>
2024-05-19 15:56:03 -0700

    Updated calcMetrics and plotMssim

--- tools/calcMetrics.m
+++ tools/calcMetrics.m
@@ -1,24 +1,30 @@
 close all;
 clear all;
 addpath('../data/simData/')
-addpath('../data/output2/threshold_0_1/')
+
 
 noisyLena = single(imread('noisyLena.png'));
 lena = imread('Lena.png');
 lena = rgb2gray(lena);
-mssim = [];
-PSNR = [];
-for iImage = 0:121
-    currentImage = table2array(readtable("denoisedImage"+string(iImage)+".csv"));
-    [mssim_0, ssim_map_0] = ssim_index(lena, currentImage);
-    mssim = [mssim, mssim_0];
+THRESHOLDS = [0.01, 0.02, 0.03, 0.05, 0.08, 0.09, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5];
 
-    currentPSNR = 10.*log10(255^2/(norm(single(lena(:))-currentImage(:)))^2);
-    PSNR = [PSNR, currentPSNR];
+for iThreshold = 1:length(THRESHOLDS)
+    pathFolder = "../data/output/threshold_"+string(THRESHOLDS(iThreshold))+"/";
+    mssim = [];
+    PSNR = [];
+    for iImage = 0:300
+       
+        currentImage = table2array(readtable(pathFolder+"denoisedImage"+string(iImage)+".csv"));
+        [mssim_0, ssim_map_0] = ssim_index(lena, currentImage);
+        mssim = [mssim, mssim_0];
+    
+        currentPSNR = 10.*log10(255^2/(norm(single(lena(:))-currentImage(:)))^2);
+        PSNR = [PSNR, currentPSNR];
+    end
+    writematrix(mssim, pathFolder+"mssim.csv");
+    writematrix(PSNR, pathFolder+"PSNR.csv");
 end
 
-writematrix(mssim, '../data/output2/threshold_0_1/mssim.csv');
-writematrix(PSNR, '../data/output2/threshold_0_1/PSNR.csv');
 
 
 
--- tools/plotMssim.m
+++ tools/plotMssim.m
@@ -1,60 +1,30 @@
 clear all;
 close all;
 
+THRESHOLDS = [0.01, 0.02, 0.03, 0.05, 0.08, 0.09, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.5];
+mmssim = [];
+psnr = [];
 
-mssim_0_05_1 = table2array(readtable("../data/output/threshold_0_1/mssim.csv"));
-mssim_0_05_2 = table2array(readtable("../data/output2/threshold_0_1/mssim.csv"));
-figure; 
-plot(mssim_0_05_1, '-o')
-hold on;
-plot(mssim_0_05_2, '-*')
+for iThreshold = 1:length(THRESHOLDS)
+    pathFolder = "../data/output/threshold_"+string(THRESHOLDS(iThreshold))+"/";
+    currentMSSIM = table2array(readtable(pathFolder+"mssim.csv"));
+    currentPSNR = table2array(readtable(pathFolder+"PSNR.csv"));
+    mmssim = [mmssim; currentMSSIM];
+    psnr = [psnr; currentPSNR];    
+end
 
-
-
-
-
-
-
-
-
-
-
-mssim_0_05 = table2array(readtable("../data/threshold_0_05/mssim.csv"));
-mssim_0_08 = table2array(readtable("../data/threshold_0_08/mssim.csv"));
-mssim_0_09 = table2array(readtable("../data/threshold_0_09/mssim.csv"));
-mssim_0_1 = table2array(readtable("../data/threshold_0_1/mssim.csv"));
-mssim_0_2 = table2array(readtable("../data/threshold_0_2/mssim.csv"));
-mssim_0_3 = table2array(readtable("../data/threshold_0_3/mssim.csv"));
-
-
-figure; 
-plot(mssim_0_05, '-o')
-hold on;
-plot(mssim_0_08,'-.')
-plot(mssim_0_09,'-.')
-plot(mssim_0_1,'-o')
-plot(mssim_0_2,'-*')
-plot(mssim_0_3,'-^')
+figure; hold on;
+for iThreshold = 1:length(THRESHOLDS)
+    plot(mmssim(iThreshold,:))
+end
+ylim([0 0.6])
 grid on;
-legend('mssim_0_05','mssim_0_08','mssim_0_09','mssim_0_1','mssim_0_2','mssim_0_3')
+legend('0.01', '0.02', '0.03', '0.05', '0.08', '0.09', '0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.5');
 
-
-
-psnr_0_05 = table2array(readtable("../data/threshold_0_05/PSNR.csv"));
-psnr_0_08 = table2array(readtable("../data/threshold_0_08/PSNR.csv"));
-psnr_0_09 = table2array(readtable("../data/threshold_0_09/PSNR.csv"));
-psnr_0_1 = table2array(readtable("../data/threshold_0_1/PSNR.csv"));
-psnr_0_2 = table2array(readtable("../data/threshold_0_2/PSNR.csv"));
-psnr_0_3 = table2array(readtable("../data/threshold_0_3/PSNR.csv"));
-
-
-figure; 
-plot(psnr_0_05, '-o')
-hold on;
-plot(psnr_0_08,'-.')
-plot(psnr_0_09,'-.')
-plot(psnr_0_1,'-o')
-plot(psnr_0_2,'-*')
-plot(psnr_0_3,'-^')
+figure; hold on;
+for iThreshold = 1:length(THRESHOLDS)
+    plot(psnr(iThreshold,:))
+end
 grid on;
-legend('psnr_0_05','psnr_0_08','psnr_0_09','psnr_0_1','psnr_0_2','psnr_0_3')
+legend('0.01', '0.02', '0.03', '0.05', '0.08', '0.09', '0.1', '0.15', '0.2', '0.25', '0.3', '0.35', '0.4', '0.5');
+
