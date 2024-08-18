a1e2da8b6c8e5a6bc36dc7192c8306a6ba82b2e2
johnfox17 <johnfox17@gmail.com>
2024-08-17 17:42:41 -0700

    Added findFuzzySimilarityImage and findSimilarityPercent

--- src/fuzzyDiffusionFilterPDDO.py
+++ src/fuzzyDiffusionFilterPDDO.py
@@ -48,31 +48,37 @@ class fuzzyDiffusionFilterPDDO:
         neighboringPixels = tree.query_radius(self.coordinateMesh, r = self.dx*self.horizon)
         self.neighboringPixels = neighboringPixels.reshape((self.Nx,self.Ny))
     
+    def loadMembershipFunction(self):
+        self.membershipFunction = np.loadtxt(self.pathToMembershipFunction, delimiter=",")
 
     def calculateGradient(self):
         self.gradient10 = signal.convolve2d(self.image, self.g10, boundary='symm')
         self.gradient01 = signal.convolve2d(self.image, self.g01, boundary='symm')
     
-    def calculateCoefficients(self):
-        coefficients = [] 
-        #K=0.5*np.mean(np.abs(self.gradient10) + np.abs(self.gradient01))
-        K=0.8
-        for iCol in range(self.Nx):
-            for iRow in range(self.Ny):
-                iGradientMagnitude = np.sqrt(self.gradient10[iRow,iCol]**2 + self.gradient01[iRow,iCol]**2)
-                #iGradientMagnitude = np.sqrt(self.gradient10[iRow,iCol]**2)
-                #coefficients.append(np.exp(-np.power(np.abs(np.divide(iGradientMagnitude,K)),2)))
-                coefficients.append(1/(1+np.divide(iGradientMagnitude,K)))
-        self.coefficients = np.pad(np.array(np.transpose(np.array(coefficients).reshape((self.Nx, self.Ny)))),int(self.horizon),mode='constant')
-        #np.savetxt('../data/outputPDDODerivative3/coefficients.csv',  self.coefficients, delimiter=",")
-        #print('Here')
-        #a = input('').split(" ")[0]
+    def findSimilarityPercent(self, pixelDifferences):
+        indexOfMembership = 2 - self.membershipFunction[pixelDifferences,0]
+        similarityMembership = []
+        for iPixel in range(len(indexOfMembership)):
+            idxMembership = indexOfMembership[iPixel]
+            if idxMembership == 2.0:
+                similarityMembership.append(1.0)
+            elif idxMembership == 1.0:
+                similarityMembership.append(0.5)
+            elif idxMembership == 0.0:
+                 similarityMembership.append(0.0)
+        similarityPercent = np.divide(np.dot(similarityMembership,indexOfMembership ),np.sum(indexOfMembership))
+        return similarityPercent
+    
+    def findFuzzySimilarityImage(self):
+        image = np.pad(self.image,int(self.horizon),mode='symmetric').astype(float)
+        similarityPercent = []
+        for iCol in range(int(self.horizon),self.Nx+2):
+            for iRow in range(int(self.horizon),self.Ny+2):
+                pixelDifferences = np.abs(image[iRow,iCol]-image[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1]).flatten().astype(int)
+                similarityPercent.append(self.findSimilarityPercent(pixelDifferences))
+        self.similarityPercent = np.pad(np.array(similarityPercent).reshape((self.Nx, self.Ny)), int(self.horizon),'constant')
+        self.similarity = np.pad(np.transpose(np.divide(signal.convolve2d(self.similarityPercent, self.GMask, boundary='symm', mode='same'),len(self.GMask.flatten()))), int(self.horizon),'constant')
 
-    def calculateGradientOfCoefficients(self):
-        self.gradientCoefficients10 = np.pad(signal.convolve2d(self.coefficients, self.g10, boundary='symm', mode='same'),int(self.horizon),'constant')
-        self.gradientCoefficients01 = np.pad(signal.convolve2d(self.coefficients, self.g01, boundary='symm', mode='same'),int(self.horizon),'constant')
-        #self.gradientCoefficients10 = np.pad(self.coefficients,int(self.horizon),'constant')
-        #self.gradientCoefficients01 = np.pad(self.coefficients,int(self.horizon),'constant')
 
     def solveRHS(self):
         #self.RHS = np.multiply(self.gradient10,self.gradientCoefficients10) + np.multiply(self.gradient01,self.gradientCoefficients01)
@@ -81,7 +87,7 @@ class fuzzyDiffusionFilterPDDO:
         gradient = self.gradient10 + self.gradient01
         for iCol in range(int(self.horizon),self.Nx+2):
             for iRow in range(int(self.horizon),self.Ny+2):
-                RHS.append(np.sum(np.multiply(np.multiply(self.GMask,self.gradient10[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1]),self.coefficients[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1]))+ np.sum(np.multiply(np.multiply(self.GMask,self.gradient01[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1]),self.coefficients[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1])))
+                RHS.append(np.sum(np.multiply(np.multiply(self.GMask,self.gradient10[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1]),self.similarity[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1]))+ np.sum(np.multiply(np.multiply(self.GMask,self.gradient01[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1]),self.similarity[iRow-int(self.horizon):iRow+int(self.horizon)+1,iCol-int(self.horizon):iCol+int(self.horizon)+1])))
                 #RHS.append(np.sum(np.multiply(np.multiply(self.rhsMask,self.gradient10[iRow-int(self.horizon-2):iRow+int(self.horizon-2)+1,iCol-int(self.horizon-2):iCol+int(self.horizon-2)+1]),self.gradientCoefficients10[iRow-int(self.horizon-2):iRow+int(self.horizon-2)+1,iCol-int(self.horizon-2):iCol+int(self.horizon-2)+1]))+ np.sum(np.multiply(np.multiply(self.rhsMask,self.gradient01[iRow-int(self.horizon-2):iRow+int(self.horizon-2)+1,iCol-int(self.horizon-2):iCol+int(self.horizon-2)+1]),self.gradientCoefficients01[iRow-int(self.horizon-2):iRow+int(self.horizon-2)+1,iCol-int(self.horizon-2):iCol+int(self.horizon-2)+1])))
         
         self.RHS = np.transpose(np.array(RHS).reshape((self.Nx,self.Ny)))
@@ -99,22 +105,14 @@ class fuzzyDiffusionFilterPDDO:
         #self.image = np.multiply(np.divide(self.image,np.max(np.absolute(self.image))),255).reshape((self.Nx, self.Ny))
         self.image = self.image.reshape((self.Nx, self.Ny))
 
-    def enhanceContrast(self):
-        image = []
-        for iCol in range(self.Nx):
-            for iRow in range(self.Ny):
-                image.append(self.contrastEnhancementLUT[int(self.image[iRow,iCol])])
-        self.image = np.transpose(np.array(image).reshape((self.Nx, self.Ny)))
 
     def solveDifferentialEquation(self):
         noisyImage = self.image
-        #print(np.shape(self.image))
         self.calculateGradient()
-        self.calculateCoefficients()
-        self.calculateGradientOfCoefficients()        
+        self.findFuzzySimilarityImage()
         self.solveRHS()
-        #print('solveRHS'+str(np.shape(self.image)))
         self.denoisedImage = noisyImage + self.dt*self.lambd*self.RHS
+
 
     def timeIntegrate(self):
         timeSteps = int(self.finalTime/self.dt)
@@ -125,26 +123,12 @@ class fuzzyDiffusionFilterPDDO:
             self.solveDifferentialEquation()           
             self.checkSaturation()
             self.normalizeTo8Bits()
-            #if iTimeStep % 100 == 0:
-            #p0, p1 = np.percentile(self.image, (10, 99.9))
-            #self.image = np.multiply(255,exposure.rescale_intensity(self.image, in_range=(p0, p1)))
-            #self.image = match_histograms(self.image, self.referenceImage)
-            #np.savetxt('../data/outputColorImage7/RHS.csv',  self.RHS, delimiter=",")
-            #print('Here')
-            #a = input('').split(" ")[0]
-            #if self.saturated: 
-            #    self.enhanceContrast()
             '''print('Here')
             np.savetxt('../data/outputColorImage/RHS2_'+str(iTimeStep)+'.csv',  self.RHS[2], delimiter=",")
             a = input('').split(" ")[0]'''
-            #image = np.divide(self.image,2**8) 
-            # Contrast stretching
-            #p0, p1 = np.percentile(image, (10, 99.9))
-            #image = np.multiply(255,exposure.rescale_intensity(image, in_range=(p0, p1)))
-            #self.image = exposure.rescale_intensity(self.image, in_range=(p0, p1)).astype(int)
-            if iTimeStep % 100 == 0:
-                image = match_histograms(self.image, self.referenceImage)
-                np.savetxt('../data/outputPDDODerivative3/'+str(iTimeStep)+'_'+'denoisedImage.csv', image)
+            #if iTimeStep % 100 == 0:
+            image = match_histograms(self.image, self.referenceImage)
+            np.savetxt('../data/output/'+str(iTimeStep)+'_'+'denoisedImage.csv', image)
             #cv2.imwrite('../data/output/'+str(iTimeStep)+'_'+'denoisedImage.jpg', self.image)
             #print(type(img_rescale))
             #a = input('').split(" ")[0]
@@ -153,6 +137,7 @@ class fuzzyDiffusionFilterPDDO:
     def solve(self, referenceImage, image):
         self.referenceImage = referenceImage
         self.image = image
+        self.loadMembershipFunction()
         self.createPDDOKernelMesh()
         self.findNeighboringPixels()
         
